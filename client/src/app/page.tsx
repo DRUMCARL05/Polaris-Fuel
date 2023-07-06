9953b3576c6ab6b959455a268fe2cd25fdb11ee8
Saket Develops <78320447+saket1225@users.noreply.github.com>
2023-07-06 18:24:24 +0530

    Made the layout match the user interface of the design.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -11,6 +11,7 @@ import {
   Transaction,
   sendAndConfirmTransaction,
 } from "@solana/web3.js";
+import { relative } from 'path';
 
 let provider: {
   disconnect(): unknown;
@@ -212,86 +213,108 @@ class page extends Component {
     return (
       <div>
         {/* NAVBAR START HERE */}
-        <div className='navBar' style={{display:"flex",width:"100%"}}>
-        <img style={{marginLeft:100}} width={200} src="https://cdn.discordapp.com/attachments/1121693555891638282/1124865911459958855/PXPtexture.png"></img>
-        <h1 style={{color:"white",marginTop:80}}>Polaris</h1>
-       
-        <button onClick={this.customerClick.bind(this)} style={{marginLeft:"360px",width:"120px",height:"60px",marginTop:65,backgroundColor:this.state.customerButtonColor,color:"white"}}>Customer</button>
-        <button onClick={this.providerClick.bind(this)} style={{marginLeft:"0px",width:"120px",height:"60px",marginTop:65,backgroundColor:this.state.providerButtonColor,color:"white"}}>Polaris Provider</button>
-       
-        <button style={{position:"absolute",right:200,top:80,width:200,height:60,backgroundColor:"#e36414",color:"white"}} id="WalletButton" onClick={this.walletClick}>Connect Wallet</button>
+        <div className='navBar' style={{display:"flex", justifyContent: "space-between", width:"100%", marginTop: -20}}>
+          <div className="logo" style={{ display: "flex"}}>
+            <img style={{marginLeft: 50, marginTop: 50}} width={75} height={75} src="https://cdn.discordapp.com/attachments/1119286494453055528/1126142304319717417/PXPbig-RedCirclePLess.png"></img>
+            <h2 style={{color:"white", marginTop:75}}>Polaris</h2>
+          </div>
+
+          <div className="toggle" style={{ display: "flex", borderRadius: 20, overflow: 'hidden', marginTop: 65, border: '2px solid #E36414', background: '#E3641480'}}>
+            <button onClick={this.customerClick.bind(this)} style={{width:"170px",height:"60px",backgroundColor:this.state.customerButtonColor,color:"#f0f0f0", border: 'none', outline: 'none', fontSize: '17px', cursor: 'pointer'}}>Customers</button>
+            <button onClick={this.providerClick.bind(this)} style={{width:"200px",height:"60px",backgroundColor:this.state.providerButtonColor,color:"#f0f0f0", border: 'none', outline: 'none', fontSize: '17px', cursor: 'pointer'}}>Polaris Provider</button>
+          </div>
+
+          <button style={{position:"relative",top:65,width:225,height:60,backgroundColor:"#e36414",color:"white", marginRight: 60, border: 'none', outline: 'none', borderRadius: '20px', fontSize: 18, cursor: 'pointer'}} id="WalletButton" onClick={this.walletClick}>Connect Wallet</button>
         </div>
         {/* NAVBAR ENDS HERE */}
 
 
        {/* MARKET UI STARTS HERE */}
+       <div style={{display:"flex", justifyContent: "center", marginTop:"75px"}}>
+          <div style={{display:"flex", border: '1px solid #6C757D', borderRadius: 15, overflow: 'hidden'}}>
+            <div style={{ display: 'flex', flexDirection: 'column', padding: 25, paddingLeft: 50, paddingRight: 50, borderRight: '1px solid #6C757D' }}>
+              <h2 style={{ color: '#000814' }}>.</h2>
+              <div className="content">
+                <h3 style={{ color: '#f0f0f0', fontWeight: 600, marginBottom: 35 }}>FOOD</h3>
+                <h3 style={{ color: '#f0f0f0', fontWeight: 600, marginBottom: 35 }}>FUEL</h3>
+                <h3 style={{ color: '#f0f0f0', fontWeight: 600, marginBottom: 35 }}>AMMO</h3>
+                <h3 style={{ color: '#f0f0f0', fontWeight: 600, marginBottom: 35 }}>TOOLS</h3>
+              </div>
+            </div>
+            
+            <div style={{ display: 'flex', flexDirection: 'column', padding: 25, paddingLeft: 50, paddingRight: 50, borderRight: '1px solid #6C757D' }}>
+              <h2 style={{ color: '#f0f0f0', fontWeight: 600 }}>{this.state.marketQtyBoxText}</h2>
+              <div className="content">
+                <h3 style={{ color: '#f0f0f0', fontWeight: 300, marginBottom: 35}}>{this.state.foodSupplyAmmount}</h3>
+                <h3 style={{ color: '#f0f0f0', fontWeight: 300, marginBottom: 35}}>{this.state.fuelSupplyAmmount}</h3>
+                <h3 style={{ color: '#f0f0f0', fontWeight: 300, marginBottom: 35}}>{this.state.ammoSupplyAmmount}</h3>
+                <h3 style={{ color: '#f0f0f0', fontWeight: 300, marginBottom: 35}}>{this.state.toolsSupplyAmmount}</h3>
+              </div>
+            </div>
 
-       <div style={{display:"flex", justifyContent: "center", marginTop:"30px"}}>
-          <div style={{display:"flex"}}>
-            <div className='marketQTYBox'>
-                <div style={{color:"white"}}>{this.state.marketQtyBoxText}</div>
-                <input disabled id="food" placeholder={"food                  "+this.state.foodSupplyAmmount} ></input>
-                <div></div>
-                <input disabled id="fuel" placeholder={"fuel                   "+this.state.fuelSupplyAmmount} ></input>
-                <div></div>
-                <input disabled id="ammo" placeholder={"ammo               "+this.state.ammoSupplyAmmount}></input>
-                <div></div>
-                <input disabled id="tools" placeholder={"tools                 "+this.state.toolsSupplyAmmount} ></input>
+            <div style={{ display: 'flex', flexDirection: 'column', padding: 25, paddingLeft: 50, paddingRight: 50, borderRight: '1px solid #6C757D' }}>
+              <h2 style={{ color: '#f0f0f0', fontWeight: 600 }}>Current Price</h2>
+              <div className="content">
+                <h3 style={{ color: '#E36414', fontWeight: 300, textAlign: 'left', marginBottom: 35}}>{this.state.foodMSRdisplay} <span style={{ color: '#f0f0f0', paddingLeft: 15 }}> Atlas</span></h3>
+                <h3 style={{ color: '#E36414', fontWeight: 300, textAlign: 'left', marginBottom: 35}}>{this.state.fuelMSRdisplay} <span style={{ color: '#f0f0f0', paddingLeft: 15 }}> Atlas</span></h3>
+                <h3 style={{ color: '#E36414', fontWeight: 300, textAlign: 'left', marginBottom: 35}}>{this.state.ammoMSRdisplay} <span style={{ color: '#f0f0f0', paddingLeft: 15 }}> Atlas</span></h3>
+                <h3 style={{ color: '#E36414', fontWeight: 300, textAlign: 'left', marginBottom: 35}}>{this.state.toolsMSRdisplay} <span style={{ color: '#f0f0f0', paddingLeft: 15 }}> Atlas</span></h3>
+              </div>
             </div>
-            <div className='currentPrices'>
-                <div style={{color:"white"}}>  Current Price</div>
-                <input disabled id="food" placeholder={"food                  "+this.state.foodMSRdisplay} ></input>
-                <div></div>
-                <input disabled id="fuel" placeholder={"fuel                   "+this.state.fuelMSRdisplay} ></input>
-                <div></div>
-                <input disabled id="ammo" placeholder={"ammo               "+this.state.ammoMSRdisplay}></input>
-                <div></div>
-                <input disabled id="tools" placeholder={"tools                 "+this.state.toolsMSRdisplay} ></input>
-            </div>
-            <div className='ammountBox'>
-                <div style={{color:"white"}}>{this.state.ammountBoxText}</div>
-                <div className='inputContainer' style={{position:"relative"}}>
-                    <input type='number' onChange={this.changeAmmount.bind(this)} id="food" placeholder='Enter Amount' 
-                          onKeyPress={event => {if(event.key === '.') event.preventDefault();}} ></input>
-                    <button style={{position:"absolute",top:0,right:0}}>Max</button>
+            
+            <div style={{ display: 'flex', flexDirection: 'column', padding: 25, paddingLeft: 50, paddingRight: 50, borderRight: '1px solid #6C757D' }}>
+              <h2 style={{ color: '#f0f0f0', fontWeight: 600 }}>{this.state.ammountBoxText}</h2>
+              <div className="content" style={{ marginTop: '13px' }}>
+                <div className="buyingAmount">
+                  <input type="text" style={{ border: 'none', fontSize: 16, background: '#1C1E20', padding: '0.8rem', outline: '#E36414', color: '#f0f0f0', borderRadius: '5px' }}  placeholder='Enter Amount' id='food' onChange={this.changeAmmount.bind(this)} onKeyPress={event => {if(event.key === '.') event.preventDefault();}}/>
+                  <button style={{ position: 'relative', left: '-2.5rem',cursor: 'pointer', background: 'none', outline: 'none', border: 'none', backgroundColor: 'none', color: '#E36414', fontSize: 12, fontWeight: 200}}>Max</button>
+                  <span style={{color: '#E36414', fontSize: 18}}>{(this.state.foodMSRdisplay*this.state.foodAmmount).toFixed(6)} <span style={{color: '#f0f0f0', paddingLeft: 15}}>  Atlas</span></span>
                 </div>
-                <div className='inputContainer' style={{position:"relative"}}>
-                    <input type='number' onChange={this.changeAmmount.bind(this)} id="fuel" placeholder='Enter Amount'
-                          onKeyPress={event => {if(event.key === '.') event.preventDefault();}} ></input>
-                    <button style={{position:"absolute",top:0,right:0}}>Max</button>
+                
+                <div style={{marginTop: 12}} className="buyingAmount">
+                  <input type="text" style={{ border: 'none', fontSize: 16, background: '#1C1E20', padding: '0.8rem', outline: '#E36414', color: '#f0f0f0', borderRadius: '5px' }}  placeholder='Enter Amount' id='fuel' onChange={this.changeAmmount.bind(this)} onKeyPress={event => {if(event.key === '.') event.preventDefault();}}/>
+                  <button style={{ position: 'relative', left: '-2.5rem',cursor: 'pointer', background: 'none', outline: 'none', border: 'none', backgroundColor: 'none', color: '#E36414', fontSize: 12, fontWeight: 200}}>Max</button>
+                  <span style={{color: '#E36414', fontSize: 18}}>{(this.state.fuelMSRdisplay*this.state.fuelAmmount).toFixed(6)} <span style={{color: '#f0f0f0', paddingLeft: 15}}>  Atlas</span></span>
                 </div>
-                <div className='inputContainer' style={{position:"relative"}}>
-                    <input type='number' onChange={this.changeAmmount.bind(this)} id="ammo" placeholder='Enter Amount'
-                          onKeyPress={event => {if(event.key === '.') event.preventDefault();}} ></input>
-                    <button style={{position:"absolute",top:0,right:0}}>Max</button>
+                
+                <div style={{marginTop: 12}} className="buyingAmount">
+                  <input type="text" style={{ border: 'none', fontSize: 16, background: '#1C1E20', padding: '0.8rem', outline: '#E36414', color: '#f0f0f0', borderRadius: '5px' }}  placeholder='Enter Amount' id='ammo' onChange={this.changeAmmount.bind(this)} onKeyPress={event => {if(event.key === '.') event.preventDefault();}}/>
+                  <button style={{ position: 'relative', left: '-2.5rem',cursor: 'pointer', background: 'none', outline: 'none', border: 'none', backgroundColor: 'none', color: '#E36414', fontSize: 12, fontWeight: 200}}>Max</button>
+                  <span style={{color: '#E36414', fontSize: 18}}>{(this.state.ammoMSRdisplay*this.state.ammoAmmount).toFixed(6)} <span style={{color: '#f0f0f0', paddingLeft: 15}}>  Atlas</span></span>
                 </div>
-                <div className='inputContainer' style={{position:"relative"}}>
-                    <input type='number' onChange={this.changeAmmount.bind(this)} id="tools" placeholder='Enter Amount'
-                          onKeyPress={event => {if(event.key === '.') event.preventDefault();}} ></input>
-                    <button style={{position:"absolute",top:0,right:0}}>Max</button>
+                
+                <div style={{marginTop: 12}} className="buyingAmount">
+                  <input type="text" style={{ border: 'none', fontSize: 16, background: '#1C1E20', padding: '0.8rem', outline: '#E36414', color: '#f0f0f0', borderRadius: '5px' }}  placeholder='Enter Amount' id='tools' onChange={this.changeAmmount.bind(this)} onKeyPress={event => {if(event.key === '.') event.preventDefault();}}/>
+                  <button style={{ fontSize: 12, position: 'relative', left: '-2.5rem',cursor: 'pointer', background: 'none', outline: 'none', border: 'none', backgroundColor: 'none', color: '#E36414', fontWeight: 200}}>Max</button>
+                  <span style={{color: '#E36414', fontSize: 18}}>{(this.state.toolsMSRdisplay*this.state.toolsAmmount).toFixed(6)} <span style={{color: '#f0f0f0', paddingLeft: 15}}>  Atlas</span></span>
                 </div>
-            </div>
-            <div style={{marginTop:"20px",marginLeft:"20px"}} className='total'>
-                <div style={{marginTop:"2px",color:'white'}} > {(this.state.foodMSRdisplay*this.state.foodAmmount).toFixed(6)}   ATLAS</div>
-                <div style={{marginTop:"2px",color:'white'}} > {(this.state.fuelMSRdisplay*this.state.fuelAmmount).toFixed(6)}   ATLAS</div>
-                <div style={{marginTop:"2px",color:'white'}} > {(this.state.ammoMSRdisplay*this.state.ammoAmmount).toFixed(6)}   ATLAS</div>
-                <div style={{marginTop:"2px",color:'white'}} > {(this.state.toolsMSRdisplay*this.state.toolsAmmount).toFixed(6)}   ATLAS</div>
+              </div>
             </div>
           </div>
       </div>
 
       {/* MARKET UI ENDS HERE */}
 
-        
-         {/* Action Button Starts HERE */}
-        <button style={{marginTop:"30px", width:"150px", marginLeft: "auto", marginRight: "auto", display: "block",width:300,height:60}} id="Send" onClick={this.callProgram}>{this.state.actionButton}</button>
-         {/* Action Button ends HERE */}
+      
+      {/* Action Button Starts HERE */}
+        <button style={{marginTop:"50px", width:"200px", marginLeft: "auto", marginRight: "auto", display: "block", height:50, fontSize: 18, color: '#E36414', background: 'none', border: '1px solid #E36414', borderRadius: 10, outline: 'none', cursor: 'pointer'}} id="Send" onClick={this.callProgram}>{this.state.actionButton}</button>
+      {/* Action Button ends HERE */}
 
 
-           {/* FOOTER STARTS HERE */}
-
-
-          {/* FOOTER ENDS HERE */}
+      {/* FOOTER STARTS HERE */}
+        <div className="footer" style={{position: 'absolute', padding: '15px 0', left: 0, width: '100vw', bottom: 0, display: 'flex', justifyContent: 'space-between', borderTop: '1px solid #EDF2F4'}}>
+          <div className="memberShipLevel" style={{display: 'flex', left: 75, position: 'relative'}}>
+            <h3 style={{color: '#f0f0f0'}}>Membership Level: </h3>
+            <div className="icon" style={{ width: '1rem', height: '1rem', borderRadius: 50, border: '5px solid #E36414', position: 'relative', top: '1rem', marginLeft: '1rem' }}></div>
+            <h3 className="rank" style={{ color: '#E36414', marginLeft: '0.5rem' }}>Gold</h3>
+          </div>
+          
+          <div className="currentPxp" style={{display: 'flex', marginRight: 75}}>
+            <h3 style={{color: '#f0f0f0'}}>Current PXP: </h3>
+            <h3 className="pxp" style={{ color: '#E36414', marginLeft: '0.5rem' }}>126,000</h3>
+          </div>
+        </div>
+      {/* FOOTER ENDS HERE */}
 
 
       </div>
