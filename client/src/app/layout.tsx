764ee3f42061f596c6b68aa7050f585d0e8b2082
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2023-10-17 00:42:13 +0500

    update-alex

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Polaris Fuel',
-  description: 'Premier Star Atlas Resource Marketplace',
-}
+  title: "Polaris Fuel",
+  description: "Premier Star Atlas Resource Marketplace",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body style={{backgroundColor:"#000814"}} className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body
+        className={`${inter.className} flex flex-col bg-[#000814] min-h-screen`}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
