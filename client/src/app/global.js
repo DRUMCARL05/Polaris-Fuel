8d0368adb3aeba6ef1c0f19bb42c11731ed1c06b
redazul <azuldevgames@gmail.com>
2024-09-04 19:14:40 -0400

    fix admin

--- client/src/app/global.js
+++ client/src/app/global.js
@@ -7,3 +7,8 @@ export const connection = new Connection('https://devnet.helius-rpc.com/?api-key
 export const feePubKey = new PublicKey("5SYuwdp6eL8rSjfRWJ45P6WRLeV9Vnegxf8p2jrJh4xb");
 export const atlasMint = new PublicKey("ATLADWy6dnnY3McjmRvuvRZHR4WjYYtGGKS3duedyBmy");
 export const ammoMint = new PublicKey("AMMUxMuL93NDbTzCE6ntjF8U6fMdtiw6VbXS3FiLfaZd");
+export const foodMint = new PublicKey("foodE1HScTH3Xr3vTWsnjLUyNumwdiQaRiDhjbGe2Cy");
+export const fuelMint = new PublicKey("fue6aCbsAjNthxmf46t3CEehrwoVcV2HU9nv1zWqSFb");
+export const toolMint = new PublicKey("too4o45Y1vDtCdMRX8tNENvjMQwwRHC7PBtk4hrp6JG");
+
+
